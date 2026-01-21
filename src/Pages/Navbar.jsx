8f53a2dc396ebab4375ef74f80488f799925eb6a
arulmozhi302-wq <arulmozhi.302@gmail.com>
2026-01-21 20:00:22 +0530

    first commit2

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -6,7 +6,6 @@ function Navbar() {
         <nav>
         <Link style={{ padding: '20px' }} to='/' >Home</Link>
         <Link style={{ padding: '20px' }} to='/User' >User</Link>
-        <Link style={{ padding: '20px' }} to='/Navbar' >Navbar</Link>
         <Link style={{ padding: '20px' }} to='/Product' >Product</Link>
         <Link style={{ padding: '20px' }} to='/ProductDetail' >ProductDetail</Link>
         </nav>
