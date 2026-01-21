a75b622dc089b6ebf67af69f032df8ad31c1ecf7
arulmozhi302-wq <arulmozhi.302@gmail.com>
2026-01-21 19:58:45 +0530

    first commit1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <a style={{ padding: '20px' }} href='#' target='_blank'>Home</a>
-        <a style={{ padding: '20px' }} href='/User' target='_blank'>User</a>
-        <a style={{ padding: '20px' }} href='/Navbar' target='_blank'>Navbar</a>
-        <a style={{ padding: '20px' }} href='/Product' target='_blank'>Product</a>
-        <a style={{ padding: '20px' }} href='/ProductDetail' target='_blank'>ProductDetail</a>
+        Home
     </div>
     
   )
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <nav>
+        <Link style={{ padding: '20px' }} to='/' >Home</Link>
+        <Link style={{ padding: '20px' }} to='/User' >User</Link>
+        <Link style={{ padding: '20px' }} to='/Navbar' >Navbar</Link>
+        <Link style={{ padding: '20px' }} to='/Product' >Product</Link>
+        <Link style={{ padding: '20px' }} to='/ProductDetail' >ProductDetail</Link>
+        </nav>
+    </div>
   )
 }
 
